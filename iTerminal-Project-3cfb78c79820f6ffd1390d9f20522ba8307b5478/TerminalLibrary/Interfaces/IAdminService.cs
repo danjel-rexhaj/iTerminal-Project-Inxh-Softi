b55e2d664116143981b524b4338ab1c11fb7d59d
Gera7d1 <152689285+Gera7d1@users.noreply.github.com>
2025-12-12 21:09:55 +0100

    Update IAdminService.cs

--- iTerminal-Project-3cfb78c79820f6ffd1390d9f20522ba8307b5478/TerminalLibrary/Interfaces/IAdminService.cs
+++ iTerminal-Project-3cfb78c79820f6ffd1390d9f20522ba8307b5478/TerminalLibrary/Interfaces/IAdminService.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,15 +7,24 @@ using TerminalLibrary.Models;
 
 namespace TerminalLibrary.Interfaces
 {
-    public  interface IAdminService
+    // Updated formatting (minor change)
+    public interface IAdminService
     {
         DataTwo GetRegisteredRoutes();
+        
         bool CheckRoute(Linja route);
+        
         bool RegisterRoute(Linja route);
+        
         PaginatedProductViewModel GetMyUnits(int userId, int page, int nisja);
+        
         bool DeleteUnit(int UnitId, int UserId);
+        
         DataTwo GetUnitAndLinja(int unitId);
+        
         Unit GetUnitById(int UnitId);
+        
         List<Linja> GetRoutes();
     }
 }
+
